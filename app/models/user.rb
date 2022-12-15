a3b646935e2a35d926b02af348facfc3afb11103
Colleen Cabedo <cmcabedo@gmail.com>
2022-12-15 15:52:32 -0500

    model relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :favorites
+    has_many :bookings, dependent: :destroy
+    has_many :listings, through: :favorites
+    has_many :listings, through: :bookings
+
+    has_secure_password  
 end
